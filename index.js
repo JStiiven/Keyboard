3dd20b66d89e3b892da23d28736c628f903e815a
JStiiven <johnortega13579@gmail.com>
2024-06-10 14:19:17 -0500

    Estudio Propio!

---
+++ index.js
@@ -0,0 +1,94 @@
+let guardado = document.querySelector("#vinculao")
+function botones(boton) {
+    guardado.innerHTML += boton;
+}
+
+function quitarString() {
+    let contenido = guardado.innerHTML
+
+    let contenidoFin = contenido.substring(0, contenido.length -1);
+
+    guardado.innerHTML = contenidoFin
+
+}
+/* function botonQ() {
+    alert("Q")
+}
+function botonW() {
+    alert("W")
+}
+function botonE() {
+    alert("E")
+}
+function botonR() {
+    alert("R")
+}
+function botonT() {
+    alert("T")
+}
+function botonY() {
+    alert("Y")
+}
+function botonU() {
+    alert("U")
+}
+function botonI() {
+    alert("I")
+}
+function botonO() {
+    alert("O")
+}
+function botonP() {
+    alert("P")
+}
+function botonA() {
+    alert("A")
+}
+function botonS() {
+    alert("S")
+}
+function botonD() {
+    alert("D")
+}
+function botonF() {
+    alert("F")
+}
+function botonG() {
+    alert("G")
+}
+function botonH() {
+    alert("H")
+}
+function botonJ() {
+    alert("J")
+}
+function botonK() {
+    alert("K")
+}
+function botonL() {
+    alert("L")
+}
+function botonÑ() {
+    alert("Apretaste laÑ")
+}
+function botonZ() {
+    alert("Z")
+}
+function botonX() {
+    alert("X")
+}
+function botonC() {
+    alert("C")
+}
+function botonV() {
+    alert("V")
+}
+function botonB() {
+    alert("B")
+}
+function botonN() {
+    alert("N")
+}
+function botonM() {
+    alert("M")
+} */
